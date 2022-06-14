b13cb919565bc9a28f6db4e1734d01a133c19f3e
nikbritoff <britov.nd@gmail.com>
2022-06-14 16:30:11 +0300

    Добавляет eslint, prettier, husky и lint-staged. Добавляет Pre-commit hook

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,38 +1,43 @@
-import { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { Theme } from '../constants/theme';
 
 type ThemeContextType = {
-  currentTheme: Theme,
-  toggleTheme: () => void,
-}
+  currentTheme: Theme;
+  toggleTheme: () => void;
+};
 
 type ThemeContextProps = {
-  children: React.ReactNode,
-}
+  children: React.ReactNode;
+};
 
 const ThemeContext = createContext<ThemeContextType>({} as ThemeContextType);
 
-const ThemeContextProvider: React.FC<ThemeContextProps> = (props) => {
+const ThemeContextProvider: React.FC<ThemeContextProps> = (
+  props: ThemeContextProps,
+) => {
   const [currentTheme, setCurrentTheme] = useState<Theme>(Theme.light);
-  
+
   const toggleTheme = () => {
     const theme = currentTheme === Theme.dark ? Theme.light : Theme.dark;
     setCurrentTheme(theme);
-  }
+  };
 
   return (
-    <ThemeContext.Provider value={{currentTheme, toggleTheme}}>
+    <ThemeContext.Provider value={{ currentTheme, toggleTheme }}>
       {props.children}
     </ThemeContext.Provider>
-)};
+  );
+};
 
 const useThemeContext = () => {
   const context = useContext(ThemeContext);
   if (!context) {
-    throw new Error('useThemeContext should be used only with ThemeContextProvider');
+    throw new Error(
+      'useThemeContext should be used only with ThemeContextProvider',
+    );
   } else {
     return context;
   }
-}
+};
 
 export { ThemeContextProvider, useThemeContext };
